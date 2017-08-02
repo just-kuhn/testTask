28da2d30fd794f7104c666e065342311a595b60a
just-kuhn <vdzeikun@gmail.com>
2017-08-03 00:36:50 +0300

    Add default migration

--- db/migrate/20170802061026_create_categories.rb
+++ db/migrate/20170802061026_create_categories.rb
@@ -1,10 +1,9 @@
 class CreateCategories < ActiveRecord::Migration[5.0]
   def change
     create_table :categories do |t|
-      t.string :name, :default => 'no-category'
+      t.string :name
 
       t.timestamps  
     end
-    Category.create(name: "no-category")
   end
 end
--- db/migrate/20170802163832_add_slug_to_categories.rb
+++ db/migrate/20170802163832_add_slug_to_categories.rb
@@ -2,5 +2,6 @@ class AddSlugToCategories < ActiveRecord::Migration[5.0]
   def change
     add_column :categories, :slug, :string
     add_index :categories, :slug, unique: true
+    Category.create(name: "no-category")
   end
 end
